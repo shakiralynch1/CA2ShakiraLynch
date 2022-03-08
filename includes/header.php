464ac28bc8b9792c8e42a90cc81e81655403b432
shakiralynch1 <92158341+shakiralynch1@users.noreply.github.com>
2022-03-08 17:44:06 +0000

    changing css and changing the structure of the homepage

--- includes/header.php
+++ includes/header.php
@@ -3,10 +3,10 @@
 <title>My PHP CRUD App</title>
 <link rel="stylesheet" href="css/mainstyle.css">
 <link rel= "stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<script src="calendar.js"></script>
+<script src="list.js"></script>
 <link rel="stylesheet" type>
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>Calendar</h1></header>
+<header><h1>Organiser</h1></header>
